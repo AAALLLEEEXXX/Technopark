7fb9a0f1329f8a595de8f7f93dc25f4ef927c2f7
AAALLLEEEXXX <aaallleeexxx918@gmail.com>
2017-10-25 19:18:17 +0300

    project

--- WhatToday/WhatToday/FirstController.swift
+++ WhatToday/WhatToday/FirstController.swift
@@ -11,18 +11,70 @@ import UIKit
 
 class FirstController: UIViewController {
     
-
+    @IBOutlet weak var menuBarButtonItem: UIBarButtonItem!
     @IBOutlet weak var firstButton: UIButton!
     @IBOutlet weak var secondButton: UIButton!
     
-    
+  var menuVC: MenuViewController!
     
 override func viewDidLoad() {
     super.viewDidLoad()
   
     firstButton.applyDesign1()
     secondButton.applyDesign3()
+    
+    menuVC = self.storyboard?.instantiateViewController(withIdentifier: "MenuVC") as! MenuViewController
+    
+    let  swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.handleSwipe))
+    swipeRight.direction = .right
+    
+    let  swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.handleSwipe))
+    swipeLeft.direction = .left
+    
+    self.view.addGestureRecognizer(swipeRight)
+    self.view.addGestureRecognizer(swipeLeft)
 }
+    
+    @objc func  handleSwipe(gesture: UISwipeGestureRecognizer){
+        switch gesture.direction {
+        case UISwipeGestureRecognizerDirection.right:
+            print("Right")
+            showMenu()
+        case UISwipeGestureRecognizerDirection.left:
+            print("Left")
+            hideMenu()
+        default: break
+        }
+    }
+    
+    @IBAction func menuBarButtonItem(_ sender: UIBarButtonItem){
+        if AppDelegate.isMenuVC {
+            showMenu()
+        } else{
+            hideMenu()
+        }
+    }
+    
+    func showMenu() {
+        UIView.animate(withDuration: 0.3) {
+            self.menuVC.view.frame = CGRect(x:0, y:60, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
+            self.addChildViewController(self.menuVC)
+            self.view.addSubview(self.menuVC.view)
+            AppDelegate.isMenuVC = false
+        }
+    }
+    
+    func hideMenu(){
+        UIView.animate(withDuration: 0.3, animations: {
+            self.menuVC.view.frame = CGRect(x: -UIScreen.main.bounds.size.width, y:60, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
+            
+        }) { (finished) in
+            self.menuVC.view.removeFromSuperview()
+            AppDelegate.isMenuVC = true
+        }
+        
+        
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
