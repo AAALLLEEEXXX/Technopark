77dcf0bc8e74f64fbb22cc083a1a208bd95752f6
neoline07 <neoline07@gmail.com>
2017-11-24 22:32:32 +0300

    Работающая фильтрация в выдаче месяц+день
    Ура!

--- test version/WhatToday/SecondController.swift
+++ test version/WhatToday/SecondController.swift
@@ -10,47 +10,42 @@ import Foundation
 import UIKit
 import RealmSwift
 
-class SecondController: UIViewController, UITableViewDelegate{
+
+class SecondController: UIViewController, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
-    
-    let massiv = [
-        Massiv(name:"08/01", flickrID: "С космодрома Байконур осуществлен запуск космического коробля Союз ТМ-18"),
-        Massiv(name:"08/01", flickrID: "Катастрофа АН-32 в Киншасе"),
-        Massiv(name:"08/01", flickrID: "Катастрофа Beechcraft в Шарлотте"),
-        Massiv(name:"08/01", flickrID: "Церемония крещения морского суперлайнера"),
-    ]
+    var dayForSearch: String?
+    var monthForSearch: String? {
+        didSet {
+            guard let monthForSearch = self.monthForSearch else {
+                return
+            }
+            
+            self.realevents = (try! Realm()).objects(EventsDB.self).filter("dataMonth = '\(monthForSearch)' && dataDay = '\(dayForSearch ?? "0")'")
+        }
+    }
 
     override func viewDidLoad() {
-        super .viewDidLoad()
-     
-        
-        }
-    
-    //func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-   //     print("You select \(massiv[indexPath.row])")
-   // }
-  //  let sravndata = ViewController()
-   // sravndata.EventsDB.adddata.dataMonth == months_arr
-    
-}
+    super.viewDidLoad()
+    }
 
-var realevents = try! Realm().objects(EventsDB.self).sorted(byKeyPath: "dataMonth", ascending: true)
+    var realevents: Results<EventsDB>?
+}
 
 extension SecondController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return realevents.count
+        return realevents?.count ?? 0
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
             else {
                 return UITableViewCell()
         }
-        let realevent = realevents[indexPath.row]
-        cell.textLabel?.text = realevent.desc
+        let realevent = realevents?[indexPath.row]
+        cell.textLabel?.text = realevent?.desc
+        
         return cell
     }
 }
-
 
 
